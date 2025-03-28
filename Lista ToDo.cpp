9f442d410eebb19523e1181410496226138a2e3a
Czajto <rogala10009@gmail.com>
2025-03-28 08:49:42 +0100

    Usunieto zbedne spacje, taby i wciecia oraz bardziej uporzadkowany kod

--- Lista ToDo.cpp
+++ Lista ToDo.cpp
@@ -11,17 +11,12 @@
 #include <cstdlib>  // dla system()
 
 /*
-
 X = NIE ZROBIONE
 ✓ = ZROBIONE
 
 RZECZY DO ZROBIENIA!!!!
-##############################
+############################
 DODANIE FUNKCJI OZNACZANIA W DANYM PLIKU RZECZY ZROBIONYCH I NIE (X)
-FUNKCJA KTORA USUWA PLIKI Z KOMPUTERA (X)
-
-MOZLIWOSC CZYSCZENIA OBECNEJ LISTY Z FUNKCJA "CZY NA PEWNO?" (✓)
-DODAWANIE A NIE NADPISYWANIE DO ISTNIEJACYCH PLIKOW Z ZADANIAMI (✓)
 ############################
 */
 
@@ -31,7 +26,6 @@ namespace fs = filesystem;
 // Deklaracja funkcji potwierdzenia
 bool potwierdzenie(const string& komunikat);
 void StworzPlik(const string& nazwa, const string& sciezka);
-
 
 void czyscEkran() {
 #ifdef _WIN32
@@ -134,7 +128,7 @@ void UsunPlik(string& nazwa, const string& path){
     try{
         if(fs::remove(sciezka)){
             cout << "Plik " << nazwa + ".txt został usuniety pomyslne." << endl;
-        }   else {
+        } else {
             cout << "Plik nie znaleziony." << endl;
         }
     } catch (const fs::filesystem_error& e) {
@@ -163,16 +157,15 @@ void wczytywanieZpliku(vector<string>& lista, string& nazwa, string& path) {
 
 void wyswietlaniePlikow(const string& sciezka) {
 
-    if (fs::is_empty(sciezka)) {
+    if (fs::is_empty(sciezka)){
         string nazwa;
         cout << "Brak plików w folderze!" << endl;
         cout << "Podaj nazwe pliku: ";
         cin >> nazwa;
         StworzPlik(nazwa, sciezka);
     }
-
     cout << "Pliki w folderze: " << sciezka << endl;
-    for (const auto& plik : fs::directory_iterator(sciezka)) {
+    for (const auto& plik : fs::directory_iterator(sciezka)){
         cout << " - " << plik.path().stem() << endl;
     }
 }
@@ -209,9 +202,7 @@ int main() {
 
     string nazwaPliku;
     vector<string> listaZadan;
-
     cout << "Proszę wybrać folder do którego beda zapisywane i pobierane pliki!" << endl;
-
     string folder = choose_folder();
     string wyborUsuniecia;
 
@@ -227,69 +218,68 @@ int main() {
         czyscEkran();
 
         switch (mainWybor) {
+            case 1:
+                folder = choose_folder();
 
-        case 1:
-            folder = choose_folder();
+                if (!folder.empty()) {
+                    cout << "Wybrano folder: " << folder << endl;
+                } else {
+                    cout << "Anulowano wybór folderu" << endl;
+                } break;
 
-            if (!folder.empty()) {
-                cout << "Wybrano folder: " << folder << endl;
-            } else {
-                cout << "Anulowano wybór folderu" << endl;
-            } break;
-
-        case 2:
-            dodajZadanie(listaZadan);
-            break;
-        case 3:
-            pokazywanieZadania(listaZadan);
-            break;
-        case 4:
-            cout << "Co chcesz zrobić?" << endl;
-            cout << "[1] Usunąć wszystkie zadania z listy " << endl;
-            cout << "[2] Usunąć pojedyncze zadania z listy wybrane przez ciebie" << endl;
-            cout << "Podaj cokolwiek aby cofnąć" << endl;
-            cin >> wyborUsuniecia;
-            if (wyborUsuniecia == "1") {
-                wyczyscCalaListe(listaZadan);
-            }
-            else if (wyborUsuniecia == "2") {
+            case 2:
+                dodajZadanie(listaZadan);
+                break;
+            case 3:
                 pokazywanieZadania(listaZadan);
-                usuwanieZadania(listaZadan);
-            }
-            break;
-        case 5:
-            pokazywanieZadania(listaZadan);
-            edytujZadanie(listaZadan);
-            break;
-        case 6:               // Zapisz do pliku
-            wyswietlaniePlikow(folder);
-            cout << "Podaj nazwe pliku do którego chcesz zapisać zadania: ";
-            cin >> nazwaPliku;
-            zapiszDoPliku(listaZadan, nazwaPliku, folder);
-            break;
-        case 7:
-            wyswietlaniePlikow(folder);
-            cout << "Z którego pliku chcesz załadowac zadania?" << endl;
-            cin >> nazwaPliku;
-            wczytywanieZpliku(listaZadan, nazwaPliku, folder);
-            break;
-        case 8:
-            wyswietlaniePlikow(folder);
-            cout << "Który plik chcesz usunąć?" << endl;
-            cout << "Wpisz Exit aby wyjść" << endl;
-            cin >> nazwaPliku;
-            if (nazwaPliku != "Exit" && nazwaPliku != "exit"){
-                UsunPlik(nazwaPliku, folder);
                 break;
-            } else {
-                czyscEkran();
+            case 4:
+                cout << "Co chcesz zrobić?" << endl;
+                cout << "[1] Usunąć wszystkie zadania z listy " << endl;
+                cout << "[2] Usunąć pojedyncze zadania z listy wybrane przez ciebie" << endl;
+                cout << "Podaj cokolwiek aby cofnąć" << endl;
+                cin >> wyborUsuniecia;
+                if (wyborUsuniecia == "1") {
+                    wyczyscCalaListe(listaZadan);
+                }
+                else if (wyborUsuniecia == "2") {
+                    pokazywanieZadania(listaZadan);
+                    usuwanieZadania(listaZadan);
+                }
                 break;
-            }
-        case 9:
-            cout << "Żegnaj!" << endl;
-            return 0;
-        default:
-            cout << "Proszę podać jedną z opcji" << endl;
+            case 5:
+                pokazywanieZadania(listaZadan);
+                edytujZadanie(listaZadan);
+                break;
+            case 6:               // Zapisz do pliku
+                wyswietlaniePlikow(folder);
+                cout << "Podaj nazwe pliku do którego chcesz zapisać zadania: ";
+                cin >> nazwaPliku;
+                zapiszDoPliku(listaZadan, nazwaPliku, folder);
+                break;
+            case 7:
+                wyswietlaniePlikow(folder);
+                cout << "Z którego pliku chcesz załadowac zadania?" << endl;
+                cin >> nazwaPliku;
+                wczytywanieZpliku(listaZadan, nazwaPliku, folder);
+                break;
+            case 8:
+                wyswietlaniePlikow(folder);
+                cout << "Który plik chcesz usunąć?" << endl;
+                cout << "Wpisz Exit aby wyjść" << endl;
+                cin >> nazwaPliku;
+                if (nazwaPliku != "Exit" && nazwaPliku != "exit"){
+                    UsunPlik(nazwaPliku, folder);
+                    break;
+                } else {
+                    czyscEkran();
+                    break;
+                }
+            case 9:
+                cout << "Żegnaj!" << endl;
+                return 0;
+            default:
+                cout << "Proszę podać jedną z opcji" << endl;
         }
     }
 }
